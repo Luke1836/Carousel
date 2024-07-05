02f16ccbf9c5c7d7d3cc2efed95ed7f4b6c1df2b
Luke1836 <codingluke2023@gmail.com>
2024-07-05 19:53:51 +0530

    String literals added

---
+++ app.js
@@ -0,0 +1,45 @@
+const menu = [
+    {
+      id: 1,
+      title: "Aurora Borealis",
+      img: "./Source/aurora.avif",
+      desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta debitis vitae officiis provident consequuntur dolore ut et odio accusantium necessitatibus, iusto quaerat earum inventore voluptate ducimus deserunt, maxime fugit.`,
+    },
+    {
+      id: 2,
+      title: "Aurora Australis",
+      img: "./Source/aurora_potrait.avif",
+      desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta debitis vitae officiis provident consequuntur dolore ut et odio accusantium necessitatibus, iusto quaerat earum inventore voluptate ducimus deserunt, maxime fugit.`,
+    },
+    {
+      id: 3,
+      title: "Mount Vesuvius",
+      img: "./Source/mount-vesuvius.jpg",
+      desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta debitis vitae officiis provident consequuntur dolore ut et odio accusantium necessitatibus, iusto quaerat earum inventore voluptate ducimus deserunt, maxime fugit.`,
+    },
+    {
+      id: 4,
+      title: "Nyiragongo",
+      img: "./Source/Nyiragongo-Volcano-Congo.avif",
+      desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta debitis vitae officiis provident consequuntur dolore ut et odio accusantium necessitatibus, iusto quaerat earum inventore voluptate ducimus deserunt, maxime fugit.`,
+    },
+    {
+      id: 5,
+      title: "Shoal of Fish",
+      img: "./Source/photo-1682686581776-b6ebee7c150e.avif",
+      desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta debitis vitae officiis provident consequuntur dolore ut et odio accusantium necessitatibus, iusto quaerat earum inventore voluptate ducimus deserunt, maxime fugit.`,
+    },
+    {
+      id: 6,
+      title: "Mount St.Helens",
+      img: "./Source/St.Helens.avif",
+      desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta debitis vitae officiis provident consequuntur dolore ut et odio accusantium necessitatibus, iusto quaerat earum inventore voluptate ducimus deserunt, maxime fugit.`,
+    },
+    {
+      id: 7,
+      title: "Waterfall",
+      img: "./Source/waterfall-1.avif",
+      desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta debitis vitae officiis provident consequuntur dolore ut et odio accusantium necessitatibus, iusto quaerat earum inventore voluptate ducimus deserunt, maxime fugit.`,
+    },
+    
+  ];
